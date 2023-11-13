b5b5d95aa5ad16a027c7d304b8444200968dd62e
Shane-Donlon <shane.donlon@live.com>
2023-11-13 16:36:18 +0000

    Nav bar expanding add

--- swarms/static/base.js
+++ swarms/static/base.js
@@ -1,1 +1,7 @@
 console.log("base.js working");
+let beekeeperMenu = document.querySelector(".beekeeper-expanding");
+let beekeeperUL = document.querySelector(".beekeeper-ul");
+beekeeperMenu.addEventListener("click", (e) => {
+  beekeeperUL.classList.toggle("opened");
+  console.log(e);
+});
