d017740c4312fbf7c6c62cb51f54b78dad8487cd
Shane-Donlon <shane.donlon@live.com>
2023-11-13 14:59:19 +0000

    AT: test urls, template routing for homepage

--- swarms/tests/test_views.py
+++ swarms/tests/test_views.py
@@ -6,8 +6,11 @@ from unauthenticated.views import testingView
 
 
 class TestUrls(SimpleTestCase):
+    """_summary_
+        Testing that the home page loads, and that the view is working as expected,
+        if correct template is used the correct View has been called
+    """
     def test_home_url_resolves(self):
-        assert 1 == 2
-        
-    def test__another_home_url_resolves(self):
-        assert 1 == 1
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "unauthenticated/unauthenticated.html")
