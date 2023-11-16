93911deba95754dcdf6ac694bc20106708c5a9cb
Shane-Donlon <shane.donlon@live.com>
2023-11-16 09:51:41 +0000

    Style add: navigation styling

--- swarms/tests/test_views.py
+++ swarms/tests/test_views.py
@@ -1,8 +1,7 @@
-from django.test import TestCase, SimpleTestCase
+from django.test import SimpleTestCase, TestCase
 from django.urls import resolve, reverse
 from unauthenticated.views import testingView
 # Create your tests here.
-
 
 
 class TestUrls(SimpleTestCase):
@@ -13,4 +12,5 @@ class TestUrls(SimpleTestCase):
     def test_home_url_resolves(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "unauthenticated/unauthenticated.html")
+        self.assertTemplateUsed(
+            response, "unauthenticated/unauthenticated.html")
