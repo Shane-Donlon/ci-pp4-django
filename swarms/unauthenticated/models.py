47663319b694d660fa9f3e5b75cc20e7e504f405
Shane-Donlon <shane.donlon@live.com>
2023-11-17 14:46:30 +0000

    Feat add: FAQ Model

--- swarms/unauthenticated/models.py
+++ swarms/unauthenticated/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Faq(models.Model):
+    summary = models.CharField(max_length=500)
+    details = models.TextField(max_length=5000)
+    status_choices = [("Shown", "Shown"), ("Hidden", "Hidden")]
+    status = models.CharField(
+        max_length=50, choices=status_choices, null=True, default="Shown")
+    created_on = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.summary}"
