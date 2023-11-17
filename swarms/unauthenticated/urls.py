b635cdb3f3e1e7997c24e7ced89535f033d3d7d3
Shane-Donlon <shane.donlon@live.com>
2023-11-17 12:04:30 +0000

    Feat Add: learn more honey bee url routing and class view

--- swarms/unauthenticated/urls.py
+++ swarms/unauthenticated/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.IndexPageView.as_view(), name="home"),
     path("about-us", views.AboutUsPageView.as_view(), name="about"),
+    path("learn-more/honeybee", views.LearnMoreHoneyBee.as_view(), name="learnhoneybee"),
 ]
--- swarms/unauthenticated/views.py
+++ swarms/unauthenticated/views.py
@@ -8,3 +8,7 @@ class IndexPageView(View):
 class AboutUsPageView(View):
     def get(self, request):
         return render(request, "unauthenticated/about-us.html")
+
+class LearnMoreHoneyBee(View):
+    def get(self, request):
+        return render(request, "unauthenticated/learn_more/honey-bee.html")
