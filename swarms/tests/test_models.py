ddd8c4f04672e699e1cd51722642daa9348e1a9e
Shane-Donlon <shane.donlon@live.com>
2023-11-17 15:39:59 +0000

    AT Test models for FAQ Added

---
+++ swarms/tests/test_models.py
@@ -0,0 +1,11 @@
+from django.test import SimpleTestCase, TestCase
+from unauthenticated.models import Faq
+
+class TestModels(TestCase):
+    
+    def test_Faq_model_create(self):
+        """_summary_
+        When a FAQ model is created default status == shown
+        """
+        question = Faq.objects.create(summary="This is a summary", details="These are details")
+        self.assertEqual(question.status, "Shown")
