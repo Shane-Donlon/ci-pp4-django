5ddd99fd0fdf3c8e6787c609c04e7c9ac1cb5fc0
Shane-Donlon <shane.donlon@live.com>
2023-11-18 11:10:18 +0000

    Views Add: return context for FAQ shown only

--- swarms/unauthenticated/views.py
+++ swarms/unauthenticated/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Faq
 # Create your views here.
 class IndexPageView(View):
+    
     def get(self, request):
-        return render(request, "unauthenticated/unauthenticated.html")
+        context = {"faq": Faq.objects.filter(status="Shown")}
+        return render(request, "unauthenticated/unauthenticated.html", context)
     
 class AboutUsPageView(View):
     def get(self, request):
