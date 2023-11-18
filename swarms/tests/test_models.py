b8622c700c3de1635b0bf3b8e1eb63365d65b798
Shane-Donlon <shane.donlon@live.com>
2023-11-18 16:40:54 +0000

    AT Model test to add and update FAQ model

--- swarms/tests/test_models.py
+++ swarms/tests/test_models.py
@@ -1,11 +1,14 @@
 from django.test import SimpleTestCase, TestCase
 from unauthenticated.models import Faq
 
+
 class TestModels(TestCase):
-    
     def test_Faq_model_create(self):
         """_summary_
         When a FAQ model is created default status == shown
         """
-        question = Faq.objects.create(summary="This is a summary", details="These are details")
+        question = Faq.objects.create(
+            summary="This is a summary", details="These are details")
         self.assertEqual(question.status, "Shown")
+        question.status = "Hidden"
+        self.assertEquals(question.status, "Hidden")
