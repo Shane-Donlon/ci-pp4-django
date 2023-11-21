813b7d4d7f6ac6fb86f8b63004da73338aba4278
Shane-Donlon <shane.donlon@live.com>
2023-11-21 11:12:48 +0000

    URL Routing update, renamed to index

--- swarms/unauthenticated/urls.py
+++ swarms/unauthenticated/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.IndexPageView.as_view(), name="home"),
+    path("", views.IndexPageView.as_view(), name="index"),
     path("about-us", views.AboutUsPageView.as_view(), name="about"),
 ]
