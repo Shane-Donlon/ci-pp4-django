7dfcec3c51ab041f169343f798f9d8262e2ed4e5
Shane-Donlon <shane.donlon@live.com>
2023-11-26 12:30:33 +0000

    Feat add: Report SwarmsCase inital model add

--- swarms/ReportSwarm/models.py
+++ swarms/ReportSwarm/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+COUNTIES = (
+       ('available', ('Available to borrow')),
+       ('borrowed', ('Borrowed by someone')),
+       ('archived', ('Archived - not available anymore')),
+   )
 # Create your models here.
+class ReportSwarmCase(models.Model):
+    # form information for Model Form
+    first_name = models.CharField(max_length=60, null=True, blank=False,)
+    last_name = models.CharField(max_length=60, null=True, blank=False)
+    email = models.EmailField(max_length=254, null=True,blank=False)
+    address1 = models.CharField(max_length=60, null=True,blank=False)
+    address2 = models.CharField(max_length=60, null=True,blank=False)
+    address3 = models.CharField(max_length=60, null=True, blank=True)
+    county = models.CharField(max_length=60, null=True, blank=True)
+    eircode = models.CharField(max_length=8, null=True, blank=False)
