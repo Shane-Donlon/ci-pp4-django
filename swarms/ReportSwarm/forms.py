b9b4092be586200d1a008a9439abadef75fe1fdc
Shane-Donlon <shane.donlon@live.com>
2023-11-26 12:29:39 +0000

    Feat add: model form for report swarm

--- swarms/ReportSwarm/forms.py
+++ swarms/ReportSwarm/forms.py
@@ -1,1 +1,9 @@
-from django import forms
+from django.forms import ModelForm
+from .models import ReportSwarmCase, COUNTIES
+class ReportSwarmForm(ModelForm):
+
+    class Meta:
+        model = ReportSwarmCase
+        fields = "__all__"
+        labels = {"first_name": "First Name",
+                  "last_name": "Last Name"}
