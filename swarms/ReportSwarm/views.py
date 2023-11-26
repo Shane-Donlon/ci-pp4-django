4cacd9d33b30fbc104be3ab135dbc2c758bc4ceb
Shane-Donlon <shane.donlon@live.com>
2023-11-26 17:01:22 +0000

    Django add: context county names updated

--- swarms/ReportSwarm/views.py
+++ swarms/ReportSwarm/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views import View
 
+counties = ["Antrim","Armagh","Carlow","Cavan","Clare","Cork","Derry","Donegal","Down","Dublin","Fermanagh","Galway","Kerry","Kildare","Kilkenny","Laois","Leitrim","Limerick","Longford","Louth","Mayo","Meath","Monaghan","Offaly","Roscommon","Sligo","Tipperary","Tyrone","Waterford","Westmeath","Wexford","Wicklow"]
 
 from .forms import ReportSwarmForm
 # Create your views here.
 class ReportSwarmView(View):
     def get(self, request):
-        context = {"form": ReportSwarmForm}
+        context = {"form": ReportSwarmForm,"counties": counties}
         return render(request, "ReportSwarm/ReportSwarm.html", context)
