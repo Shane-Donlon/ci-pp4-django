8effd20b164fff5c0e3dddb166ff840050ba315d
Shane-Donlon <shane.donlon@live.com>
2023-11-29 16:54:31 +0000

    Model add: stage 1 of form complete, including phone number

--- swarms/ReportSwarm/models.py
+++ swarms/ReportSwarm/models.py
@@ -1,18 +1,23 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from phonenumber_field.modelfields import PhoneNumberField
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
-COUNTIES = (
-       ('available', ('Available to borrow')),
-       ('borrowed', ('Borrowed by someone')),
-       ('archived', ('Archived - not available anymore')),
-   )
+# County_Choices = [("Antrim","Antrim"),("Armagh","Armagh"),("Carlow","Carlow")]
+# "Antrim","Armagh","Carlow","Cavan","Clare","Cork","Derry","Donegal","Down","Dublin","Fermanagh","Galway","Kerry","Kildare","Kilkenny","Laois","Leitrim","Limerick","Longford","Louth","Mayo","Meath","Monaghan","Offaly","Roscommon","Sligo","Tipperary","Tyrone","Waterford","Westmeath","Wexford","Wicklow"
 # Create your models here.
 class ReportSwarmCase(models.Model):
     # form information for Model Form
-    first_name = models.CharField(max_length=60, null=True, blank=False,)
+    first_name = models.CharField(max_length=60, null=True, blank=False, )
     last_name = models.CharField(max_length=60, null=True, blank=False)
+    phone =  PhoneNumberField(max_length=12, blank=False, region="IE")
     email = models.EmailField(max_length=254, null=True,blank=False)
     address1 = models.CharField(max_length=60, null=True,blank=False)
     address2 = models.CharField(max_length=60, null=True,blank=False)
     address3 = models.CharField(max_length=60, null=True, blank=True)
-    county = models.CharField(max_length=60, null=True, blank=True)
-    eircode = models.CharField(max_length=8, null=True, blank=False)
+    county = models.CharField(max_length=60, null=True, blank=False)
+    eircode = models.CharField(max_length=8, null=True, blank=False,validators=[MinLengthValidator(7),])
+    location = models.CharField(max_length=8,choices = [("outside", "Outside"), ("inside", "Inside")])
+    description = models.TextField(max_length=10000, null=True, blank=False)
+    createdOn = models.DateTimeField(auto_now_add=True, )
+    updatedOn = models.DateTimeField(auto_now=True,)
