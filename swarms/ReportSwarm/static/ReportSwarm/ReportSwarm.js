e03cb51887beba9fe4e54dab6224a11a7df45b71
Shane-Donlon <shane.donlon@live.com>
2023-11-29 12:53:52 +0000

    Script add: HTML Form validation on next btn click

--- swarms/ReportSwarm/static/ReportSwarm/ReportSwarm.js
+++ swarms/ReportSwarm/static/ReportSwarm/ReportSwarm.js
@@ -1,9 +1,12 @@
 // form input css selector for all input in form
-let AllInputs = document.querySelectorAll("fieldset input");
+let PersonalDetailsInputs = document.querySelectorAll(
+  ".personal-details-fieldset input"
+);
 let confirmationCheckbox = document.querySelector("#checkbox");
-let formWrapper = document.querySelectorAll("fieldset");
-
-AllInputs.forEach((input) => {
+let formWrapper = document.querySelectorAll(".form-group");
+let NextBtn = document.querySelector("#next");
+let eircodeField = document.querySelector("#id_eircode");
+PersonalDetailsInputs.forEach((input) => {
   /**  on page load disable all the input fields for the form, this will be toggled with an event listener
    *as an image is required in Step2 of the form, I am seeking confirmation that the user understands this before proceeding, 
    and that this image upload will not be a surprise for the user when reaching step2
@@ -18,7 +21,7 @@ confirmationCheckbox.addEventListener("input", (e) => {
   if (confirmationCheckbox.checked) {
     value = !value;
   }
-  AllInputs.forEach((input) => {
+  PersonalDetailsInputs.forEach((input) => {
     input.disabled = value;
   });
 });
@@ -42,7 +45,7 @@ function addRequiredAsterisksToLabels() {
   });
 }
 
-AllInputs.forEach((input) => {
+PersonalDetailsInputs.forEach((input) => {
   input.addEventListener("change", (event) => {
     /** 
      * 
@@ -53,40 +56,27 @@ then wait for 3 seconds and remove the error message.
 
  */
     let textValue = input.value.trim().length;
-
-    if (!textValue) {
-      event.target.nextElementSibling.classList.remove("display-none");
-      event.target.nextElementSibling.classList.add("errors");
-
-      event.target.nextElementSibling.innerText = `${event.target.labels[0].innerText} is required`;
-      setTimeout(() => {
-        event.target.nextElementSibling.innerText = "";
-        event.target.nextElementSibling.classList.remove("errors");
-        event.target.nextElementSibling.classList.add("display-none");
-      }, 3000);
-    } else if (
-      !event.target.validity.valid &&
-      event.target.nextElementSibling.classList.contains("display-none")
-    ) {
-      event.target.nextElementSibling.classList.remove("display-none");
-      event.target.nextElementSibling.classList.add("errors");
-      event.target.nextElementSibling.innerText = `${event.target.validationMessage}`;
-      setTimeout(() => {
-        event.target.nextElementSibling.innerText = "";
-        event.target.nextElementSibling.classList.remove("errors");
-        event.target.nextElementSibling.classList.add("display-none");
-      }, 3000);
-    }
+    generalValidation(event, textValue, input, 3000);
   });
 });
 
-let eircodeField = document.querySelector("#id_eircode");
-let NextBtn = document.querySelector("#next");
 NextBtn.addEventListener("click", (e) => {
-  checkEircode(eircodeField.value);
+  let countyValue = document.querySelector("#id_county").value;
+  validateCounty(countyValue);
+  checkEircode(eircodeField.value, eircodeField);
+  validateRequredInputPersonalDetails(PersonalDetailsInputs);
+
+  if (
+    validateCounty(countyValue) &&
+    checkEircode(eircodeField.value, eircodeField) &&
+    validateRequredInputPersonalDetails(PersonalDetailsInputs)
+  ) {
+    // all form fields passed validation
+    // open the next section of the form
+  }
 });
 
-function checkEircode(eircode) {
+function checkEircode(eircode, eircodeSelector) {
   /** 
 Eircode format should be 3 characters space 4 characters
 eg
@@ -94,7 +84,11 @@ E98 PO89
 if E98PO89 convert to E98 PO89
 OR if not uppercase convert to uppercase
  */
-
+  let correct = false;
+  if (eircode.length < 7) {
+    // if less than 7 characters do nothing
+    return;
+  }
   let wordArray = eircode.split("");
 
   if (wordArray[3] != " ") {
@@ -111,8 +105,117 @@ OR if not uppercase convert to uppercase
     let codeAsString = correctCode.toString();
     codeAsString = codeAsString.replaceAll(",", "");
     codeAsString = codeAsString.toUpperCase();
-    eircodeField.value = codeAsString;
+    eircodeSelector.value = codeAsString;
+    correct = true;
   } else {
-    eircodeField.value = eircodeField.value.toUpperCase();
+    eircodeSelector.value = eircodeField.value.toUpperCase();
+    correct = true;
   }
+  return correct;
+}
+
+function validateCounty(countyValue) {
+  countyValue = countyValue.trim();
+  let inList = false;
+  let allCounties = [
+    "Antrim",
+    "Armagh",
+    "Carlow",
+    "Cavan",
+    "Clare",
+    "Cork",
+    "Derry",
+    "Donegal",
+    "Down",
+    "Dublin",
+    "Fermanagh",
+    "Galway",
+    "Kerry",
+    "Kildare",
+    "Kilkenny",
+    "Laois",
+    "Leitrim",
+    "Limerick",
+    "Longford",
+    "Louth",
+    "Mayo",
+    "Meath",
+    "Monaghan",
+    "Offaly",
+    "Roscommon",
+    "Sligo",
+    "Tipperary",
+    "Tyrone",
+    "Waterford",
+    "Westmeath",
+    "Wexford",
+    "Wicklow",
+  ];
+
+  allCounties.forEach((current) => {
+    if (current.toLowerCase() === countyValue.toLowerCase()) {
+      return (inList = true);
+    }
+  });
+
+  if (!inList) {
+    let countiesErrors = document.querySelector(".countiesErrors");
+    countiesErrors.classList.remove("display-none");
+    countiesErrors.classList.add("errors");
+    countiesErrors.innerText = "Please select a valid county from the list";
+    setTimeout(() => {
+      countiesErrors.innerText = "";
+      countiesErrors.classList.remove("errors");
+      countiesErrors.classList.add("display-none");
+    }, 3000);
+  }
+  return inList;
+}
+
+function generalValidation(event, textValue, input, timeOutInMs) {
+  if (!textValue && input.hasAttribute("required")) {
+    event.target.nextElementSibling.classList.remove("display-none");
+    event.target.nextElementSibling.classList.add("errors");
+
+    event.target.nextElementSibling.innerText = `${event.target.labels[0].innerText} is required`;
+    setTimeout(() => {
+      event.target.nextElementSibling.innerText = "";
+      event.target.nextElementSibling.classList.remove("errors");
+      event.target.nextElementSibling.classList.add("display-none");
+    }, timeOutInMs);
+  } else if (
+    !event.target.validity.valid &&
+    event.target.nextElementSibling.classList.contains("display-none")
+  ) {
+    event.target.nextElementSibling.classList.remove("display-none");
+    event.target.nextElementSibling.classList.add("errors");
+    event.target.nextElementSibling.innerText = `${event.target.validationMessage}`;
+    setTimeout(() => {
+      event.target.nextElementSibling.innerText = "";
+      event.target.nextElementSibling.classList.remove("errors");
+      event.target.nextElementSibling.classList.add("display-none");
+    }, timeOutInMs);
+  }
+}
+
+function validateRequredInputPersonalDetails(listOfInputs) {
+  let valid = [];
+  let correct = false;
+  listOfInputs.forEach((input) => {
+    if (!input.validity.valid) {
+      valid.push(false);
+      input.nextElementSibling.classList.remove("display-none");
+      input.nextElementSibling.classList.add("errors");
+      input.nextElementSibling.innerText = `${input.validationMessage}`;
+      setTimeout(() => {
+        input.nextElementSibling.innerText = "";
+        input.nextElementSibling.classList.remove("errors");
+        input.nextElementSibling.classList.add("display-none");
+      }, 3000);
+    }
+  });
+  if (valid.length === 0) {
+    correct = true;
+  }
+  return correct;
 }
