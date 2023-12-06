31eac8b02ae030f23b66f568c4aab8f8378fa2b7
Shane-Donlon <shane.donlon@live.com>
2023-12-06 11:27:51 +0000

    DB Variable updated to test prod

--- swarms/swarms/settings.py
+++ swarms/swarms/settings.py
@@ -98,9 +98,9 @@ WSGI_APPLICATION = 'swarms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-# }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 # uncomment this when running tests
 DATABASES = {
     'default': {
