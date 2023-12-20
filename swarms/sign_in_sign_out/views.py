d5221e13c795ab598e73ba43973286ad441bb584
Shane-Donlon <shane.donlon@live.com>
2023-12-20 13:53:38 +0000

    Profile form GET & POST ADD, Login Required add

--- swarms/sign_in_sign_out/views.py
+++ swarms/sign_in_sign_out/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .forms import ProfileForm
+from django.views import View
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your views here.
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+@method_decorator(login_required, name='dispatch')
+class ProfileFormView(View):
+    
+    def get(self, request):
+
+        profile = get_object_or_404(Profile, user=request.user)
+        form = ProfileForm(instance=request.user)
+ 
+        context = {"profile": profile,
+                   "form":form}
+        return render(request, "sign_in_sign_out/sign_in_sign_out.html", context)
+    
+    def post(self, request,*args, **kwargs):
+       
+        form = ProfileForm(request.POST,instance=request.user.profile) 
+
+        if form.is_valid():
+            form.save()
+            print("form saved")
+            return render(request, "sign_in_sign_out/sign_in_sign_out.html",)
+        else:
+            print("form not valid")
+            context = {"form":form}
+            return render(request, "sign_in_sign_out/sign_in_sign_out.html",context)
