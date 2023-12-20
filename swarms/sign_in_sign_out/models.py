1945747fcd26d705ec91a2f46095bd4445dbb4b6
Shane-Donlon <shane.donlon@live.com>
2023-12-20 16:26:43 +0000

    Model add: Profile model

--- swarms/sign_in_sign_out/models.py
+++ swarms/sign_in_sign_out/models.py
@@ -1,16 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from .managers import CustomUserManager
+from django.contrib.auth.models import User
 
-class MyUser(AbstractUser):
-    username = None
-    email = models.EmailField(("email address"), unique=True)
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
-    verbose_name = "user"
-    verbose_name_plural = "users"
-    
-    objects = CustomUserManager()
-    def __str__(self):
-        return self.email
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='profile',on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50, verbose_name="First Name", null=True, blank=False)
+    last_name = models.CharField( max_length=150, verbose_name="Last Name", null=True, blank=False)
+    phone = models.CharField(max_length=20, null=True, blank=False)
+    county = models.CharField(max_length=50, null=True, blank=False)
+
+    def __str(self):
+        return f"{self.first_name} {self.last_name}"
