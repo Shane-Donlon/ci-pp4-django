f3d2b8a3293dcd20bcb374ad91cbbdce8475c744
Shane-Donlon <shane.donlon@live.com>
2023-12-20 16:23:23 +0000

    Views update: Ticket can be created via post request in template

--- swarms/report_swarm/views.py
+++ swarms/report_swarm/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
 from django.views import View
-from .forms import ReportSwarmCase
+from .forms import ReportSwarmForm
 counties = ["Antrim","Armagh","Carlow","Cavan","Clare","Cork","Derry","Donegal","Down","Dublin","Fermanagh","Galway","Kerry","Kildare","Kilkenny","Laois","Leitrim","Limerick","Longford","Louth","Mayo","Meath","Monaghan","Offaly","Roscommon","Sligo","Tipperary","Tyrone","Waterford","Westmeath","Wexford","Wicklow"]
 
 from .forms import ReportSwarmForm
 # Create your views here.
 class ReportSwarmView(View):
     def get(self, request):
-        context = {"form": ReportSwarmForm,"counties": counties}
+        context = {"form": ReportSwarmForm(),"counties": counties}
         return render(request, "report_swarm/report_swarm.html", context)
     
     def post(self, request):
-        form = ReportSwarmCase(request.POST)
-        if form.is_valid():
-            form.clean()
+        form = ReportSwarmForm(request.POST, request.FILES)
+  
+        if form.is_valid():            
             form.save()
             return render(request, "report_swarm/thanks.html", )
-        context = {"form": ReportSwarmForm(request.POST),"counties": counties}
-        return render(request, "report_swarm/report_swarm.html", context )
+        else:
+            context = {"form": ReportSwarmForm(request.POST),"counties": counties,"errors":form.errors}
+            return render(request, "report_swarm/report_swarm.html", context )
