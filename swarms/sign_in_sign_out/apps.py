1ad15c4714688a338a7e171591e3f844b03e698a
Shane-Donlon <shane.donlon@live.com>
2023-12-20 13:51:04 +0000

    Signals ready add for app

--- swarms/sign_in_sign_out/apps.py
+++ swarms/sign_in_sign_out/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_save
 
 class SignInSignOutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sign_in_sign_out'
+    
+    def ready(self):
+        from .signals import create_profile
+        from django.contrib.auth.models import User
+        post_save.connect(create_profile, sender=User)
+        from .signals import update_profile
+        post_save.connect(update_profile, sender=User)
