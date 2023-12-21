59c46c96f86b871c3eb1b3d81c59535e92305780
Shane-Donlon <shane.donlon@live.com>
2023-12-21 16:40:22 +0000

    Views add: Superuser view for all open tickets add

--- swarms/tickets/views.py
+++ swarms/tickets/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views import View
+from django_tables2 import SingleTableView, tables, RequestConfig
 # Create your views here.
+
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required,permission_required
+from report_swarm.models import ReportSwarmCase
+
+class ticketTable(tables.Table):
+    class Meta:
+        model = ReportSwarmCase
+        exclude = ["description","image"]
+
+@method_decorator(login_required, name='dispatch')
+class SuperUserView(SingleTableView,View):
+    def get(self, request):
+        """_summary_
+    if user is super user go to all tickets templates, else redirect back to profile page
+
+        """
+        if request.user.is_superuser:
+            allOpenTickets =ReportSwarmCase.objects.filter(status="Open")
+            table = ticketTable(allOpenTickets,template_name = "django_tables2/bootstrap5-responsive.html")
+            RequestConfig(request).configure(table)
+            context = {"tables":table}
+            return render(request, "tickets/tickets.html",context)
+        else:
+            return redirect("profile")
+        
+        
