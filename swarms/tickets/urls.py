4faa32968c0f9504809323717578725ca3828c55
Shane-Donlon <shane.donlon@live.com>
2023-12-22 16:19:31 +0000

    url add: singular ticket url

--- swarms/tickets/urls.py
+++ swarms/tickets/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("status/open", views.OpenTickets.as_view(), name="openTickets"),
     path("status/resolved", views.ResolvedTickets.as_view(), name="resolvedTickets"),
     path("status/closed-not-solved", views.UnResolvedClosedTickets.as_view(), name="resolvedTickets"),
+    path("<int:ticketID>",views.TicketView.as_view(), name="ticketView")
 ]
