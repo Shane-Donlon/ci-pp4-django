faef717c2e6eb5cf5939b7d5263a3773ad05b2ea
Shane-Donlon <shane.donlon@live.com>
2023-12-22 15:35:41 +0000

    Django filter add: Open ticket fields exlcluding status as tickets are open

--- swarms/tickets/filters.py
+++ swarms/tickets/filters.py
@@ -5,4 +5,9 @@ from report_swarm.models import ReportSwarmCase
 class TicketFilter(FilterSet):
     class Meta:
         model = ReportSwarmCase
-        fields = ["status", "assignee"]
+        fields = ["status", "assignee","first_name"]
+
+class OpenTicket(FilterSet):
+    class Meta:
+        model = ReportSwarmCase
+        fields = ["assignee","first_name"]
