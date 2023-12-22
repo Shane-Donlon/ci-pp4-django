f1c01c2a9aab7d3de15e3f05f7836ecde3c64ebe
Shane-Donlon <shane.donlon@live.com>
2023-12-22 15:34:52 +0000

    View add: all open tickets view login required

--- swarms/tickets/views.py
+++ swarms/tickets/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django_tables2 import SingleTableView, tables, RequestConfig
 # Create your views here.
@@ -6,7 +6,7 @@ from django_tables2 import SingleTableView, tables, RequestConfig
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from report_swarm.models import ReportSwarmCase
-from .filters import TicketFilter
+from .filters import TicketFilter,OpenTicket
 
 class ticketTable(tables.Table):
     class Meta:
@@ -17,24 +17,42 @@ class ticketTable(tables.Table):
         }
 
 @method_decorator(login_required, name='dispatch')
-class SuperUserView(SingleTableView,View):
+class AllTicketView(SingleTableView,View):
     def get(self, request):
         """_summary_
     if user is super user go to all tickets templates, else redirect back to profile page
 
         """
         if request.user.is_superuser:
-
             allTickets =ReportSwarmCase.objects.all()
             CaseFilter = TicketFilter(request.GET, queryset=allTickets)
-    
-            # if there is a filtered queryset we are remaking the varaible data with the filtered data
+            # if there is a filtered queryset we are remaking the variable data with the filtered data
             allTickets = CaseFilter.qs
             table = ticketTable(allTickets,template_name = "django_tables2/bootstrap5-responsive.html")
             RequestConfig(request).configure(table)
+            table.paginate(page=request.GET.get("page", 1), per_page=10)
             context = {"tables":table,"filter":CaseFilter,}
             return render(request, "tickets/allTickets.html",context)
         else:
             return redirect("profile")
         
+@method_decorator(login_required, name='dispatch')
+class OpenTickets(SingleTableView,View):
+    def get(self, request):
+        """_summary_
+    if user is super user go to all tickets templates, else redirect back to profile page
+
+        """
+        if request.user.is_superuser:
+            openTickets =ReportSwarmCase.objects.filter(status="Open")
+            CaseFilter = OpenTicket(request.GET, queryset=openTickets)
+            # if there is a filtered queryset we are remaking the variable data with the filtered data
+            openTickets = CaseFilter.qs
+            table = ticketTable(openTickets,template_name = "django_tables2/bootstrap5-responsive.html")
+            RequestConfig(request).configure(table)
+            table.paginate(page=request.GET.get("page", 1), per_page=10)
+            context = {"tables":table,"filter":CaseFilter,}
+            return render(request, "tickets/allTickets.html",context)
+        else:
+            return redirect("profile")
         
