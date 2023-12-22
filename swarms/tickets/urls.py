a7888e90b71e197bc7b985f845731048f38d0a90
Shane-Donlon <shane.donlon@live.com>
2023-12-22 15:45:14 +0000

    Urls routing add resolved and unresolved tickets

--- swarms/tickets/urls.py
+++ swarms/tickets/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path("all", views.AllTicketView.as_view(), name="allTickets"),
     path("status/open", views.OpenTickets.as_view(), name="openTickets"),
+    path("status/resolved", views.ResolvedTickets.as_view(), name="resolvedTickets"),
+    path("status/closed-not-solved", views.UnResolvedClosedTickets.as_view(), name="resolvedTickets"),
 ]
