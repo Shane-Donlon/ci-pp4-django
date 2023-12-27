93d1f3a791eb482ea5c1fba7082e2bbf145903d1
Shane-Donlon <shane.donlon@live.com>
2023-12-27 13:24:21 +0000

    URLS add: ticket urls add to ticket app

--- swarms/tickets/urls.py
+++ swarms/tickets/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path("all", views.AllTicketView.as_view(), name="allTickets"),
     path("status/open", views.OpenTickets.as_view(), name="openTickets"),
     path("status/resolved", views.ResolvedTickets.as_view(), name="resolvedTickets"),
-    path("status/closed-not-solved", views.UnResolvedClosedTickets.as_view(), name="resolvedTickets"),
+    path("status/closed-not-solved", views.UnResolvedClosedTickets.as_view(), name="unresolvedTickets"),
     path("<int:ticketID>",views.TicketView.as_view(), name="ticketView")
 ]
