46608b855e25d2eaee716ed35d69d1e6a0c85a27
Shane-Donlon <shane.donlon@live.com>
2023-12-27 13:23:32 +0000

    URLS add: Beekeeper URL add to base urls

--- swarms/swarms/urls.py
+++ swarms/swarms/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("unauthenticated.urls")),
     path("accounts/", include("allauth.urls")),
-    path("report-a-swarm", include("ReportSwarm.urls")),
+    path("report-a-swarm", include("report_swarm.urls")),
+    path("beekeeper/", include("sign_in_sign_out.urls")),
+    path("beekeeper/tickets/", include("tickets.urls")),
 ]
