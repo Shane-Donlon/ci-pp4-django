3e25278ecb4655c5c534ba82e213ee3a88195380
Shane-Donlon <shane.donlon@live.com>
2023-12-28 14:04:20 +0000

    Script add: Hamburger menu events all pages

--- swarms/static/base.js
+++ swarms/static/base.js
@@ -1,10 +1,14 @@
-console.log("base.js working");
-let beekeeperMenu = document.querySelector(".beekeeper-expanding");
-let beekeeperUL = document.querySelector(".beekeeper-ul");
-beekeeperMenu.addEventListener("click", (e) => {
-  beekeeperUL.classList.toggle("opened");
-  console.log(e);
-});
-
+let hamburgerMenu = document.querySelector(".menu-icon");
 document.querySelector(".copyright-year").textContent =
   new Date().getFullYear();
+
+hamburgerMenu.addEventListener("click", (e) => {
+  let nav = document.querySelector(".nav");
+  let body = document.querySelector("body");
+  nav.classList.toggle("menu-open");
+  if (nav.classList.contains("menu-open")) {
+    body.style.overflow = "hidden";
+  } else {
+    body.style.removeProperty("overflow");
+  }
+});
