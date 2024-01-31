ee896b243972dbb3649f612e4d7c846bec3e6caf
Shane-Donlon <shane.donlon@live.com>
2024-01-31 14:30:40 +0000

    Script add: Ticket delete request with confirmation refactored

--- swarms/tickets/static/tickets/singularTicketAdmin.js
+++ swarms/tickets/static/tickets/singularTicketAdmin.js
@@ -1,7 +1,8 @@
 let allFields = document.querySelectorAll(".form-div :not(label)");
 let submitBtnAdmin = document.querySelector(".submit-description");
 let checkboxAllFields = document.querySelector("#checkboxAllFields");
-let deleteBtn = document.querySelector(".delete-ppi-btn");
+let deleteBtnPersonalInfo = document.querySelector(".delete-ppi-btn");
+let deleteTicketBtn = document.querySelector(".delete-ticket-btn");
 checkboxAllFields.addEventListener("change", (e) => {
   if (checkboxAllFields.checked) {
     allFields.forEach((field) => {
@@ -9,20 +10,22 @@ checkboxAllFields.addEventListener("change", (e) => {
         field.removeAttribute("disabled");
         submitBtnAdmin.classList.remove("display-none");
 
-        deleteBtn.classList.remove("display-none");
+        deleteBtnPersonalInfo.classList.remove("display-none");
+        deleteTicketBtn.classList.remove("display-none");
       }
     });
   } else {
     allFields.forEach((field) => {
       field.setAttribute("disabled", "");
       submitBtnAdmin.classList.add("display-none");
-      deleteBtn.classList.add("display-none");
+      deleteBtnPersonalInfo.classList.add("display-none");
+      deleteTicketBtn.classList.add("display-none");
     });
   }
 });
 
-deleteBtn.addEventListener("click", (e) => {
-  displayConfirmation();
+deleteBtnPersonalInfo.addEventListener("click", (e) => {
+  displayConfirmationDeletePersonalInfo();
 });
 
 function deleteInfo() {
@@ -36,7 +39,7 @@ function deleteInfo() {
   });
 }
 
-const displayConfirmation = () => {
+const displayConfirmationDeletePersonalInfo = () => {
   let response = confirm(
     "You will delete all user personal info. This cannot be undone"
   );
@@ -46,6 +49,27 @@ const displayConfirmation = () => {
     postTicket(generateTicketData()).then((message) => {
       let responseMessage = Object.values(message)[0];
       displayMessage(responseMessage);
+    });
+  }
+};
+
+deleteTicketBtn.addEventListener("click", (e) => {
+  deleteTicketWithConfirmation();
+});
+const deleteTicket = async (link) => {
+  let data = await makeRequest(link, "delete");
+  return data;
+};
+
+const deleteTicketWithConfirmation = () => {
+  let response = confirm("You will delete this ticket, this cannot be undone");
+
+  if (response) {
+    deleteTicket(link).then((message) => {
+      let responseMessage = Object.values(message)[0];
+      if (responseMessage.toLowerCase() === "delete") {
+        redirect();
+      }
     });
   }
 };
