450c9bb9590b7a2941882023c68012af984f0e65
Shane-Donlon <shane.donlon@live.com>
2024-01-31 11:56:44 +0000

    Script add: delete PPI for GDPR and dipslay message for admin only

---
+++ swarms/tickets/static/tickets/singularTicketAdmin.js
@@ -0,0 +1,48 @@
+let allFields = document.querySelectorAll(".form-div :not(label)");
+let submitBtnAdmin = document.querySelector(".submit-description");
+let checkboxAllFields = document.querySelector("#checkboxAllFields");
+let deleteBtn = document.querySelector(".delete-ppi-btn");
+checkboxAllFields.addEventListener("change", (e) => {
+  if (checkboxAllFields.checked) {
+    allFields.forEach((field) => {
+      if (field.hasAttribute("disabled")) {
+        field.removeAttribute("disabled");
+        submitBtnAdmin.classList.remove("display-none");
+
+        deleteBtn.classList.remove("display-none");
+      }
+    });
+  } else {
+    allFields.forEach((field) => {
+      field.setAttribute("disabled", "");
+      submitBtnAdmin.classList.add("display-none");
+      deleteBtn.classList.add("display-none");
+    });
+  }
+});
+
+deleteBtn.addEventListener("click", (e) => {
+  deleteInfo();
+  displayConfirmation();
+});
+
+function deleteInfo() {
+  let allLabels = document.querySelectorAll(".user-details-fieldset label");
+  allLabels.forEach((label) => {
+    if (label.innerText.toLowerCase() != "county") {
+      label.nextElementSibling.value = "";
+    }
+  });
+}
+
+const displayConfirmation = () => {
+  let response = confirm(
+    "You will delete all user personal info. This cannot be undone"
+  );
+};
+if (response) {
+  postTicket(generateTicketData()).then((message) => {
+    let responseMessage = Object.values(message)[0];
+    displayMessage(responseMessage);
+  });
+}
