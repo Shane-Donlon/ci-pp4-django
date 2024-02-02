22b203108bd24eb953c66466c31180204ccf107b
Shane-Donlon <shane.donlon@live.com>
2024-02-02 12:11:34 +0000

    Python code styling update

--- swarms/unauthenticated/models.py
+++ swarms/unauthenticated/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Faq(models.Model):
     summary = models.CharField(max_length=500)
     details = models.TextField(max_length=5000)
@@ -8,5 +10,6 @@ class Faq(models.Model):
     status = models.CharField(
         max_length=50, choices=status_choices, null=True, default="Shown")
     created_on = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return f"{self.summary}"
--- swarms/unauthenticated/views.py
+++ swarms/unauthenticated/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.views import View
+
 from .models import Faq
+
 # Create your views here.
+
+
 class IndexPageView(View):
-    
+
     def get(self, request):
-        context = {"faq": Faq.objects.filter(status="Shown"),}
+        context = {"faq": Faq.objects.filter(status="Shown"), }
         return render(request, "unauthenticated/unauthenticated.html", context)
-    
+
+
 class AboutUsPageView(View):
     def get(self, request):
         return render(request, "unauthenticated/about-us.html")
-
