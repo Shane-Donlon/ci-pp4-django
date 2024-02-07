886a644a0e77154587a03b598905faefcbc5d261
Shane-Donlon <shane.donlon@live.com>
2024-02-07 17:32:53 +0000

    Script add: table children check, and remove table if no children replace with h2

--- swarms/tickets/static/tickets/tickets.js
+++ swarms/tickets/static/tickets/tickets.js
@@ -27,3 +27,16 @@ function delayedFn(cb, delay = 1000) {
 const updateRequest = delayedFn((searchForm) => {
   searchForm.submit();
 });
+
+if (document.body.contains(document.querySelector("table"))) {
+  let table = document.querySelector("table");
+  if (!table.children[1].children.length) {
+    // if table does not have children remove the element and replace with h2
+    table.remove();
+    let main = document.querySelector("main");
+    let h2 = document.createElement("h2");
+    h2.innerText = "There are no tickets to show";
+    h2.className = "no-tickets-h2";
+    main.append(h2);
+  }
+}
