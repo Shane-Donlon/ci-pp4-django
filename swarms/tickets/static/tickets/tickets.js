a73b55d2526d4e744971f5f9ef513a1ed489fad0
Shane-Donlon <shane.donlon@live.com>
2024-02-07 17:34:41 +0000

    Script update: DOMcontentloaded event listener

--- swarms/tickets/static/tickets/tickets.js
+++ swarms/tickets/static/tickets/tickets.js
@@ -28,15 +28,17 @@ const updateRequest = delayedFn((searchForm) => {
   searchForm.submit();
 });
 
-if (document.body.contains(document.querySelector("table"))) {
-  let table = document.querySelector("table");
-  if (!table.children[1].children.length) {
-    // if table does not have children remove the element and replace with h2
-    table.remove();
-    let main = document.querySelector("main");
-    let h2 = document.createElement("h2");
-    h2.innerText = "There are no tickets to show";
-    h2.className = "no-tickets-h2";
-    main.append(h2);
+window.addEventListener("DOMContentLoaded", (e) => {
+  if (document.body.contains(document.querySelector("table"))) {
+    let table = document.querySelector("table");
+    if (!table.children[1].children.length) {
+      // if table does not have children remove the element and replace with h2
+      table.remove();
+      let main = document.querySelector("main");
+      let h2 = document.createElement("h2");
+      h2.innerText = "There are no tickets to show";
+      h2.className = "no-tickets-h2";
+      main.append(h2);
+    }
   }
-}
+});
