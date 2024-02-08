3420684dac64cd4c1179a83914932ba211bc777d
Shane-Donlon <shane.donlon@live.com>
2024-02-08 16:42:37 +0000

    Fi PEP8 Code updated

--- swarms/tickets/urls.py
+++ swarms/tickets/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path("all", views.AllTicketView.as_view(), name="allTickets"),
     path("status/open", views.OpenTickets.as_view(), name="openTickets"),
-    path("status/resolved", views.ResolvedTickets.as_view(), name="resolvedTickets"),
+    path("status/resolved",
+         views.ResolvedTickets.as_view(), name="resolvedTickets"),
     path("status/closed-not-solved",
          views.UnResolvedClosedTickets.as_view(), name="unresolvedTickets"),
     path("<int:ticketID>", views.TicketView.as_view(), name="ticketView")
