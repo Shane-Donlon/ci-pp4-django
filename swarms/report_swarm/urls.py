fee87619f29931e4df74361635a720d9a1d3edf0
Shane-Donlon <shane.donlon@live.com>
2024-02-08 20:16:51 +0000

    Fix url namespace update

--- swarms/report_swarm/urls.py
+++ swarms/report_swarm/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ReportSwarmView.as_view(), name="reportSwarm"),
+    # convert urls to snake_case and check all files
+    path("", views.ReportSwarmView.as_view(), name="report_swarm"),
 ]
