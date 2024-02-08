32982c2fbe6e8e3677d581829404a59507aa7a82
Shane-Donlon <shane.donlon@live.com>
2024-02-08 23:39:56 +0000

    Fix removed comments

--- swarms/report_swarm/urls.py
+++ swarms/report_swarm/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # convert urls to snake_case and check all files
-    path("", views.ReportSwarmView.as_view(), name="report_swarm"),
+     path("", views.ReportSwarmView.as_view(), name="report_swarm"),
 ]
