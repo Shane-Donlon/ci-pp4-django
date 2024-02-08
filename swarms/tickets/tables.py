5a2baf99a74ef190368631aad52c639f2fe70a65
Shane-Donlon <shane.donlon@live.com>
2024-02-08 16:46:00 +0000

    Fix Code PEP8 Updated

--- swarms/tickets/tables.py
+++ swarms/tickets/tables.py
@@ -4,8 +4,10 @@ from report_swarm.models import ReportSwarmCase
 
 
 class ticketTable(tables.Table):
-    id = LinkColumn('ticketView', text=lambda record: record.id, args=[A('pk')], attrs={
-        "a": {"style": "color: #0055f2;"}})
+    id = LinkColumn('ticketView',
+                    text=lambda record: record.id,
+                    args=[A('pk')], attrs={
+                        "a": {"style": "color: #0055f2;"}})
 
     class Meta:
         model = ReportSwarmCase
